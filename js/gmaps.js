7b79536578f8cf18b6185d4589c4bbbe882a6917
MisterJuul <34219530+MisterJuul@users.noreply.github.com>
2018-08-20 22:10:19 +0200

    Toevoegen Blogs, nieuwsbrieven, prijzen aangepast, gmaps goed gemaakt

--- js/gmaps.js
+++ js/gmaps.js
@@ -1,5 +1,5 @@
 function initMap() { 
-  var uluru = {lat: 52.815754, lng: 6.515109}; 
+  var uluru = {lat: 52.815753, lng: 6.515144}; 
   var map = new google.maps.Map(document.getElementById('googleMap'), { 
     zoom: 12,
     center: uluru 
